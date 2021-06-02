6a6b0a4b15bd2f9e422542d89e76448ce7a6d2fb
Renge Miyauchi <renge@nyanpa.su>
2021-06-02 16:27:50 +0800

    forgot to update model

--- app/model.py
+++ app/model.py
@@ -1,16 +1,24 @@
 from datetime import datetime
-from odmantic import Model, Field
+from odmantic import Model, Field, Reference
+
+class Channel(Model):
+    channel_id:     int
+    channel_name:   str
+    alias:          str
+    guild:          str
+    guild_id:       int
+    subscribed:     bool = True
 
 class Image(Model):
     filename:       str
     filepath:       str
     attachment_id:  int
-    guild:          str
-    guild_id:       int
-    channel:        str
-    channel_id:     int
+    channel:        Channel = Reference()
     username:       str
     user_num:       str
     user_id:        int
     message_id:     int
-    created_at:     datetime = Field(default_factory = datetime.utcnow)
+    created_at:     datetime
+    retrieved_at:   datetime = Field(default_factory = datetime.utcnow)
+    deleted:        bool = False
+    
