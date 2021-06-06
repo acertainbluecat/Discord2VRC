fd003304c59d74f14c841640a25235de977b16e9
Renge Miyauchi <renge@nyanpa.su>
2021-06-06 17:01:09 +0800

    change ints to strings in models

--- app/common/models.py
+++ app/common/models.py
@@ -3,23 +3,23 @@ from odmantic import Model, Field, Reference
 
 
 class ChannelModel(Model):
-    channel_id: int
+    channel_id: str
     channel_name: str
     alias: str
     guild: str
-    guild_id: int
+    guild_id: str
     subscribed: bool = True
 
 
 class ImageModel(Model):
     filename: str
     filepath: str
-    attachment_id: int
+    attachment_id: str
     channel: ChannelModel = Reference()
     username: str
     user_num: str
-    user_id: int
-    message_id: int
+    user_id: str
+    message_id: str
     created_at: datetime
     retrieved_at: datetime = Field(default_factory=datetime.utcnow)
     deleted: bool = False
